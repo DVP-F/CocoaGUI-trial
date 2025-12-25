2a8ec555f97b7d65e232888fc99706fce530e660
DVP-F <carnx@duck.com>
2025-12-25 20:50:49 +0000

    andddd log had faulty value type

--- size_monitor.py
+++ size_monitor.py
@@ -4,7 +4,7 @@ class monitors:
 	def monitor(objects: list[object]):
 		while monitors.monitoring:
 			for obj in objects:
-				print(f"{type(obj)}: {obj} - {asizeof(obj)}MB")
+				print(f"{type(obj)}: {obj} - {asizeof(obj)}B")
 			sleep(5)
 def start_monitor(objects: list[object]): monitors.monitoring = True; threading.Thread(target=monitors.monitor, args=(objects,)).start()
 def stop_monitor(): monitors.monitoring = False
